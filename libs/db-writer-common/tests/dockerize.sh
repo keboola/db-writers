53cb2165918ffa3aa46865274781289841fe9c5e
Miroslav Čillík <miro@keboola.com>
2018-04-25 12:15:04 +0200

    tests: dockerize larger timeout

--- libs/db-writer-common/tests/dockerize.sh
+++ libs/db-writer-common/tests/dockerize.sh
@@ -5,4 +5,4 @@ export DOCKERIZE_VERSION="v0.3.0"
 wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
     && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
-dockerize -wait tcp://mysql:3306
+dockerize -wait tcp://mysql:3306 -timeout 30s
