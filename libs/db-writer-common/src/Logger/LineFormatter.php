f82af506ee6c0f97a0693e1374753bd1eb78e695
Miroslav Čillík <miro@keboola.com>
2019-06-25 15:28:08 +0200

    fix: compatibility with monolog 1.24

--- libs/db-writer-common/src/Logger/LineFormatter.php
+++ libs/db-writer-common/src/Logger/LineFormatter.php
@@ -8,12 +8,12 @@ use Keboola\Csv\CsvFile;
 
 class LineFormatter extends \Monolog\Formatter\LineFormatter
 {
-    protected function normalize($data)
+    protected function normalize($data, $depth = 0)
     {
         if ($data instanceof CsvFile) {
             return "csv file: " . $data->getFilename();
         } else {
-            return parent::normalize($data);
+            return parent::normalize($data, $depth);
         }
     }
 }
