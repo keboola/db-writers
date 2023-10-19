90203f847817def317f5874dc433ec786b237be4
Ondra <ondra.jodas@gmail.com>
2023-10-19 22:11:46 +0200

    finish Config class

--- libs/db-writer-config/src/Config.php
+++ libs/db-writer-config/src/Config.php
@@ -2,15 +2,23 @@
 
 declare(strict_types=1);
 
-namespace MyComponent;
+namespace Keboola\DbWriterConfig;
 
 use Keboola\Component\Config\BaseConfig;
+use Keboola\DbWriterConfig\Exception\UserException;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 class Config extends BaseConfig
 {
-    // @todo implement your custom getters
-    public function getFoo(): string
+    /**
+     * @param mixed[] $config
+     */
+    protected function setConfig(array $config): void
     {
-        return $this->getStringValue(['parameters', 'foo']);
+        try {
+            parent::setConfig($config);
+        } catch (InvalidConfigurationException $e) {
+            throw new UserException($e->getMessage(), 0, $e);
+        }
     }
 }
