1df0db96af78ba5f05b546ccaf74213ef03b0590
Ondra <ondra.jodas@gmail.com>
2023-11-21 23:00:06 +0100

    finish SshTunnel tests

--- libs/db-writer-common/tests/phpunit/SshTunnelTest.php
+++ libs/db-writer-common/tests/phpunit/SshTunnelTest.php
@@ -4,34 +4,60 @@ declare(strict_types=1);
 
 namespace Keboola\DbWriter\Tests;
 
+use Generator;
 use Keboola\DbWriter\TestsTraits\CloseSshTunnelsTrait;
 use Keboola\DbWriter\Writer\SshTunnel;
 use Keboola\DbWriterConfig\Configuration\ValueObject\DatabaseConfig;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\Test\TestLogger;
+use Symfony\Component\Process\Process;
+use Throwable;
 
 class SshTunnelTest extends TestCase
 {
     use CloseSshTunnelsTrait;
 
-    public function tearDown(): void
+    public function setUp(): void
     {
-        parent::tearDown();
+        parent::setUp();
         $this->closeSshTunnels();
     }
 
     public function testOpenTunnel(): void
     {
-        $logger = new TestLogger();
-
-        $sshTunnel = new SshTunnel($logger);
-
-        $databaseConfig = DatabaseConfig::fromArray(self::getDatabaseConfig());
-
-        $newConfig = $sshTunnel->createSshTunnel($databaseConfig);
+        $configArray = self::getDatabaseConfig();
+        $newConfig = $this->openTunnel($configArray);
 
         self::assertEquals('127.0.0.1', $newConfig->getHost());
         self::assertEquals('33006', $newConfig->getPort());
+        self::assertTrue(self::checkIfTunnelIsOpen());
+    }
+
+    public function testDisabledSSHConfig(): void
+    {
+        $configArray = self::getDatabaseConfig();
+        $configArray['ssh']['enabled'] = false;
+
+        $newConfig = $this->openTunnel($configArray);
+
+        self::assertEquals((string) getenv('COMMON_DB_HOST'), $newConfig->getHost());
+        self::assertEquals((string) getenv('COMMON_DB_PORT'), $newConfig->getPort());
+        self::assertFalse(self::checkIfTunnelIsOpen());
+    }
+
+    /**
+     * @param class-string<object> $expectException
+     * @dataProvider invalidConfigDataProvider
+     */
+    public function testInvalidConfig(array $config, string $expectException, string $expectExceptionMessage): void
+    {
+        try {
+            $this->openTunnel($config);
+            self::fail('Exception should be thrown');
+        } catch (Throwable $e) {
+            self::assertInstanceOf($expectException, $e);
+            self::assertEquals($expectExceptionMessage, $e->getMessage());
+        }
     }
 
     private static function getDatabaseConfig(): array
@@ -51,6 +77,36 @@ class SshTunnelTest extends TestCase
                 ],
                 'sshHost' => 'sshproxy',
             ],
+        ];
+    }
+
+    private static function checkIfTunnelIsOpen(): bool
+    {
+        $process = new Process(['sh', '-c', 'pgrep ssh | wc -l']);
+        $process->mustRun();
+
+        $count = (int) $process->getOutput();
+        return $count > 0;
+    }
+
+    private function openTunnel(array $configArray): DatabaseConfig
+    {
+        $logger = new TestLogger();
+        $sshTunnel = new SshTunnel($logger);
+
+        $databaseConfig = DatabaseConfig::fromArray($configArray);
+
+        return $sshTunnel->createSshTunnel($databaseConfig);
+    }
+
+    public function invalidConfigDataProvider(): Generator
+    {
+        $config = self::getDatabaseConfig();
+        $config['ssh']['keys']['#private'] = '';
+        yield 'ssh-host-missing' => [
+            $config,
+            'Keboola\DbWriter\Exception\UserException',
+            'Key must not be emptyRetries count: 5',
         ];
     }
 }
